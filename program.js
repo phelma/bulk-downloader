fed77a7893dd55c50d4623e360f3f65422987c61
Phil Helm <philip.helm@omgplc.com>
2015-01-19 13:32:37 +0000

    working better

--- program.js
+++ program.js
@@ -1,4 +1,5 @@
-var inputFile = 'head.txt';
+'use strict';
+var inputFile = 'fall11_urls.txt';
 var outDir = 'out';
 
 ////////////////////////////////////
@@ -7,34 +8,57 @@ var fs = require('fs');
 var http = require('http');
 var through = require('through');
 var split = require('split');
-
-var downloadFile = function(url, fileName) {
-	console.log('downloadFile got: ' + url + ' ' + fileName);
-	var fileName = fileName || Date.now() + '.jpg';
-	var request = http.get(url, function(resp) {
-		console.log('status: ' + resp.statusCode);
-		if (resp.statusCode = 200) {
-			console.log('out to: ' + __dirname + '/' + outDir + '/' + fileName)
-			var file = fs.createWriteStream(__dirname + '/' + outDir + '/' + fileName + '.jpg');
-			console.log('pipe to file');
-			resp.pipe(file);
-		}
-	});
-};
+var url = require('url');
 
 //example
 // downloadFile('http://i.imgur.com/tYPQKZJ.jpg', 'file1.jpg');
 
+var splitTabs = through(function(buf) {
+	var item = buf.toString().split('\t');
+	this.queue(item);
+});
+
+var downloadStream = through(function(item) {
+	if (url.parse(item[1]).protocol === 'http:') {
+		console.log('downloadStream ' + item);
+		var fileName = item[0] || Date.now() + '.jpg';
+		var request = http.get(item[1], function(resp) {
+			if (resp.statusCode === 200) {
+				console.log("200 for " + fileName);
+				var file = fs.createWriteStream(__dirname + '/' + outDir + '/' + fileName + '.jpg')
+				resp.pipe(file);
+				// this.queue([fileName, resp]);
+			} else {
+				console.log('status ' + resp.statusCode);
+			}
+		}).on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
+	} else {
+		console.log('Not http so skipping: ' + item);
+	}
+}, function() {
+	console.log('downloadStream end');
+});
+
+var fileWrite = through(function(buf) {
+	var file = fs.createWriteStream(__dirname + '/' + outDir + '/' + buf[0] + '.jpg')
+	buf[1].pipe(file);
+});
+
+
+// var downloadFile = function(url, fileName) {
+// 	var fileName = fileName || Date.now() + '.jpg';
+// 	var request = http.get(url, function(resp) {
+// 		if (resp.statusCode = 200) {
+// 			var file = fs.createWriteStream(__dirname + '/' + outDir + '/' + fileName + '.jpg');
+// 			resp.pipe(file);
+// 		}
+// 	});
+// };
+
 var fileStream = fs.createReadStream(__dirname + '/' + inputFile);
 fileStream
 	.pipe(split())
-	.pipe(through(function(buf) {
-		var line = buf.toString();
-		if (line) {
-			console.log('got line: ' + line);
-			console.log('gonna download ' + line.split('\t')[1] + ' ' + line.split('\t')[0]);
-			this.queue(
-				downloadFile(line.split('\t')[1], line.split('\t')[0])
-			);
-		}
-	}));
+	.pipe(splitTabs)
+	.pipe(downloadStream);
