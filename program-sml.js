fa9ed9e172358e937001f06b6ba2136a620b24b6
Phil Helm <philip.helm@omgplc.com>
2015-01-27 18:14:35 +0000

    it works

--- program-sml.js
+++ program-sml.js
@@ -2,44 +2,68 @@
 var fs = require('fs'),
     request = require('request'),
     through = require('through'),
-    split = require('split');
-    // var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head100.txt';
-    // var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head1k.txt';
-    var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head10k.txt';
-    // var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head100k.txt';
+    split = require('split'),
+    log = require('loglevel');
+// var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head100.txt';
+// var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head1k.txt';
+// var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head10k.txt';
+var urlList = 'https://raw.githubusercontent.com/phelma/bulk-downloader/master/head100k.txt';
 
-var requestLimit = 20,
+log.setLevel('error');
+
+var totalRequests  = process.argv[2] || 1000,
+    requestLimit   = process.argv[3] || 5,
     activeRequests = 0,
-    httpTimeout = 10;
+    httpTimeout    = 10; // seconds
+
+var line = 0;
 
 var splitByTab = through(function(buf) {
+    if (line ++ >= totalRequests) {
+        this.push(null);
+    } else {
     var item = buf.toString().split('\t');
     this.push(item);
+    }
 });
+
+var downloads = {
+    total: 0,
+    success: 0,
+    error: [],
+    startTime: new Date().getTime(),
+    endTime: 0,
+    duration: function(){
+        return (this.endTime - this.startTime) + 'ms';
+    }
+}
 
 var downloadStream = through(function(item) {
     // item is array [ filename , URL ]
-    console.log(activeRequests + ' active requests\n');
+    log.info(activeRequests + ' active requests');
     if (item[1]) {
         if (activeRequests++ >= requestLimit) {
             splitByTab.pause();
             this.pause();
         }
-        console.log('Requesting ' + item[1]);
+        downloads.total++;
+        log.info('Requesting ' + item[1]);
         request
             .get({
                 url: item[1],
                 timeout: httpTimeout * 1000
             })
             .on('error', function(err) {
-                console.log('\nError: ' + err.message + '\n' + item[1] + '\n');
+                log.warn("Error: " + err.message);
+                log.error(downloads.error.push(item) + '\t' + item[0] + '\t' + item[1]);
                 if (--activeRequests < requestLimit) {
                     this.resume();
                     splitByTab.resume();
                 }
             })
             .on('response', function(response) {
-                console.log('\nSucess: ' + item[1] + '\n');
+                log.info('Sucess: ' + item[0] + '.jpg' + ' <= ' + item[1]);
+                downloads.success++;
                 if (--activeRequests < requestLimit) {
                     this.resume();
                     splitByTab.resume();
