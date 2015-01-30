1bb436038e51baf702111dd2a04717ba8fc5a89f
Phil Helm <philip.helm@omgplc.com>
2015-01-30 16:34:01 +0000

    cleaning up empty files!

--- download-wget2.sh
+++ download-wget2.sh
@@ -2,28 +2,15 @@
 NUMBER=0
 while read FILENAME URL
 do
-    wget -nc -O "./out/$FILENAME" "$URL" --user-agent="Cyberdog/2.0 (Macintosh; 68k)" -nv --timeout=2 --tries=1 --read-timeout=0.5 &   # So `wget` runs in background
+    wget --no-clobber --output-document="./out/$FILENAME" "$URL" --user-agent="Cyberdog/2.0 (Macintosh; 68k)" --no-verbose --timeout=2 --tries=1 &   # So `wget` runs in background
     NUMBER=$((NUMBER + 1))
-    if [ $NUMBER -gt 20 ]
+    if [ $NUMBER -gt 25 ]
     then
         wait   # wait all background process to finish
         NUMBER=0
     fi
-done < head100k.txt
+done < head100.txt
 wait
 
-echo "Cleaning up non JPG files"
-curr=`pwd`
-
-a=1
-for i in *; do
-    new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-    mv "$i" "$new"
-    a=$((a+1))
-done
-
-file * | grep -v "JPEG" | grep -o "^[^:]*" | xargs rm -f
-
-echo "Back to $curr"
-
-cd $curr
+echo "Cleaning up $(find . -type f -empty | wc -l) empty files"
+find ./out -type f -empty -exec rm {} \;
